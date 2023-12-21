b1ce3f148208277be35bea301046a8aa138eb131
David Levinson <dlevinson5@gmail.com>
2023-12-21 15:32:54 -0500

    cs50/io/2020 week 1 projects

---
+++ ai/2020/x/projects/1/knights/puzzle.py
@@ -0,0 +1,99 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+
+# Symbol
+# Not
+# And
+# Or
+# Implication
+# Sentence
+
+# In each of the above puzzles, each character is either a knight or a knave. Every sentence spoken by a knight is true, and every sentence spoken by a knave is false.
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+
+# AKnight = I am both a knight and a knave.
+# AKnave = I am not both a knight and a knave.
+knowledge0 = And(
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    Implication(AKnight, And(AKnight, AKnave)), # Given AKnight, Knight and Knave
+    Implication(AKnave, Not(And(AKnight, AKnave))) # Given AKnave, Not Knight and Knave
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight))))
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+
+# CASE B = KNIGHT
+#   THEN C = KNAVE
+#   THEN A = KNAVE ("A said 'I am a knave'." is False)
+
+# CASE B = KNAVE
+#   THEN C = KNIGHT
+#   THEN A = KNIGHT ( "A is a knight." = TRUE)
+
+knowledge3 = And(
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    And(Or(CKnight, CKnave), Not(And(CKnight, CKnave))),
+    Implication(BKnight, CKnave),
+    Implication(BKnave, CKnight),
+    Implication(CKnight, AKnight),
+    Implication(CKnave, AKnave),
+    Implication(BKnight, And(Implication(AKnight, AKnave), Implication(AKnave, AKnight))),
+)
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
